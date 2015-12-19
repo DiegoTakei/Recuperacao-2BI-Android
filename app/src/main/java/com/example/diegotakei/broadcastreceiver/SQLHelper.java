f5856be201730dfdd5c523314cdfed3f3dfb0108
Diego Takei <diegotakei1586@gmail.com>
2015-12-19 14:51:40 -0300

    Ajustes no SQLHelper

--- app/src/main/java/com/example/diegotakei/broadcastreceiver/SQLHelper.java
+++ app/src/main/java/com/example/diegotakei/broadcastreceiver/SQLHelper.java
@@ -11,27 +11,23 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class SQLHelper extends SQLiteOpenHelper {
 
     public static final String TB_AVALIACAO = "TB_AVALIACAO";
-    public static final String ID = "_id";
-    public static final String NOME_AVALIADOR = "avaliador";
-    public static final String TITULO_TRABALHO = "titulo";
-    public static final String NOTA = "nota";
-    public static final String COMENTARIO = "comentario";
+
     public static final int VERSAO = 1;
 
-    private static final String DATABASE_NAME = "commments.db";
     private static final int DATABASE_VERSION = 1;
 
-    // Database creation sql statement
     private static final String DATABASE_CREATE = "create table usuario(_id " +
             "integer primary key autoincrement," +
             " avaliador text not null, titulo text not null, nota text not null, comentario text not null);";
 
     public SQLHelper(Context context) {
+
         super(context, TB_AVALIACAO,null,VERSAO);
     }
 
     @Override
     public void onCreate(SQLiteDatabase database) {
+
         database.execSQL(DATABASE_CREATE);
     }
 
